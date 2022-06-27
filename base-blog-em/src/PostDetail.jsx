59bb231cf13de13d78a1ddfd1deb43a4bdeddc65
minchodang <minsu910725@gmail.com>
2022-06-27 09:17:54 +0900

    Feat: 리액트 쿼리 페이지 네이션 추가

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,19 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(
+    ["comment", post.id],
+    () => fetchComments(post.id)
+  );
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Errors</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
